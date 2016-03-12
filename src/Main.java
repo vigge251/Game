2d8111cc11e33937533434b9d3ea2b4cbc358e91
Victor <vijoh251@gmail.com>
2016-03-12 18:59:56 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Victor on 2015-12-11.
+ */
+public class Main {
+
+    private void initializeGui() {
+        System.out.println("Hej hej");
+    }
+
+    public Main(){
+        initializeGui();
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
